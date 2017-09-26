f4550b2a9771dc225583cf510faf94a855ed4863
Maggie Cheung <mc@maggiecheung.ca>
2017-09-26 19:10:43 -0400

    Updated functionality - removed date stamp, created chatbot element to respond to questions about time.

--- client/app.js
+++ client/app.js
@@ -3,13 +3,20 @@ var socket = io();
 $("button").on('click', function() {
   var text = $("#message").val();
   var who = $("#initials").val();
+
+  // Catch case of empty identity
+  if (who < 1) {
+    who = "Anonymous";
+  }
   
-  socket.emit('message', "[" + (new Date()) + "]" + who + ": " + text);
+  // socket library creates (emits) an event to the server
+  socket.emit('message', who + ": " + text);
   $('#message').val('');
   
   return false;
 });
 
+// upon receiving a message event from the server, write to client's view
 socket.on('message', function (msg) {
   $('<li>').text(msg).appendTo('#history');
 });
